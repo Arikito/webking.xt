afe0c3fa93b142faf7e662c59761e783097fc408
devxtua <dev.xt.ua@gmail.com>
2017-11-27 19:48:54 +0200

    Revert "Парсер заморские поддарки"
    This reverts commit 2f8d99a61cb9da4830405baf831d210433c33c5d.

--- ~core/model/parser_c.php
+++ ~core/model/parser_c.php
@@ -157,10 +157,9 @@ class Parser {
 		$data[0] = trim($data[0]);
 		$data[1] = trim($data[1]);
 		$base_url = 'http://zamorskiepodarki.com';
-		$url = sprintf($base_url.'/search/?search=%s', $data[0]);
-		print_r('<pre> url: '.$url.'</pre>');
+		$url = sprintf($base_url.'/shop/?searchPhrase=%s', $data[0]);
 		if($pre_parsed_html = $this->parseUrl($url)){
-			$link = $pre_parsed_html->find('row products .product-layout .product-block .product-block .name');
+			$link = $pre_parsed_html->find('.goodName');
 			$url = $base_url.$link[0]->attr['href'];
 		}
 		if($Products->SetFieldsByRewrite(G::StrToTrans($data[1]))){
@@ -175,16 +174,12 @@ class Parser {
 		if($parsed_html = $this->parseUrl($url)){
 			// Получаем артикул товара
 			$product['sup_comment'] = trim($data[0]);
-			print_r('<pre> Арт пос: '.$product['sup_comment'].'</pre>');
 			// Получаем название товара
-			//$product['name'] = trim($data[1]);
-			$product['name'] = $parsed_html->find('itemprop="name"', 0)->plaintext;
-			print_r('<pre> Название: '.$product['name'].'</pre>');
+			$product['name'] = trim($data[1]);
 			// Получаем описание товара
 			// $product['descr'] = $parsed_html->find('[itemprop="description"]', 0)->plaintext;
 			// Получаем цену товара
 			$product['price_opt_otpusk'] = $product['price_mopt_otpusk'] = trim($data[2]);
-			print_r('<pre> Цена: '.$product['price_opt_otpusk'].'</pre>');
 			// Получаем характеристики товара
 			// foreach($parsed_html->find('.fullDescriptionConteiner table .product__feature tr') as $element){
 			// 	$caption = str_replace(':', '', trim($element->children(0)->plaintext));
